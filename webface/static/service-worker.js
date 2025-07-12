841dfafa502b4347a18a76de4fcd2517d0f0a1ca
Arianna Method <theariannamethod@gmail.com>
2025-07-12 11:34:11 +0300

    Add in-app Suppertime pages and reading flow

--- webface/static/service-worker.js
+++ webface/static/service-worker.js
@@ -5,7 +5,12 @@ self.addEventListener('install', (e) => {
                 '/',
                 '/static/index.html',
                 '/static/style.css',
-                '/static/chat.js'
+                '/static/chat.js',
+                '/static/suppertime_v1.4.html',
+                '/static/suppertime_v1.6.html',
+                '/static/resonance.html',
+                '/static/page.css',
+                '/static/page.js'
             ]);
         })
     );
